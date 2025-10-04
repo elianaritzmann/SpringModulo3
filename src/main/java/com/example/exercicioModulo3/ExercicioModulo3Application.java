55e9a7673b4316e8f4d568993c3ded167375e140
Eliana Ritzmann <eliana.cristinas2@hotmail.com>
2025-10-04 16:14:57 -0300

    primeiro commit

---
+++ src/main/java/com/example/exercicioModulo3/ExercicioModulo3Application.java
@@ -0,0 +1,13 @@
+package com.example.exercicioModulo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioModulo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioModulo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exercicioModulo3/ExercicioModulo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercicioModulo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioModulo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
